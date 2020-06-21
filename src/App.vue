075ef0f16239c05dcfe3e180b07fb5e2cd2aa9bd
szdytom <szdytom@163.com>
2020-06-21 16:50:42 +0800

    install vue router

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hello!
+    <router-link to="/1231">1231</router-link>
+    <router-view>
+    </router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import App from './App.vue';
 import ElementUI from 'element-ui';
 
+import HelloWorld from './components/HelloWorld.vue'
+
 Vue.config.productionTip = false
+Vue.use(Router);
 Vue.use(ElementUI);
 
+const router = new Router({
+	mode: 'history',
+	routes: [{
+        path: "/:id",
+        component: HelloWorld
+    }]
+});
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	router,
+	render: h => h(App),
+}).$mount('#app');
