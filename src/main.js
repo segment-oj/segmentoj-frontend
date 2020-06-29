038e853e767468e793612cf8bbe2cea19ea7f3b1
szdytom <szdytom@163.com>
2020-06-29 18:28:30 +0800

    add async import (broken)

--- src/main.js
+++ src/main.js
@@ -1,29 +1,14 @@
 import Vue from 'vue';
-import Router from 'vue-router';
 import App from './App.vue';
-import ElementUI from 'element-ui';
-
-import HelloWorld from './components/HelloWorld.vue';
-import page_not_found from './components/404.vue';
 
 Vue.config.productionTip = false
-Vue.use(Router);
-Vue.use(ElementUI);
 
-const router = new Router({
-	mode: 'history',
-	routes: [{
-        path: "/"
-    }, {
-        path: "/:id",
-        component: HelloWorld
-    }, {
-        path: "*",
-        component: page_not_found
-    }]
+const router = await import('./router');
+import('element-ui').then(() => {
+    Vue.use(Element);
 });
 
 new Vue({
-	router,
+    router,
 	render: h => h(App),
 }).$mount('#app');
---
+++ src/router.js
@@ -0,0 +1,18 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+Vue.use(Router)
+
+let router = new Router({
+    mode: 'history',
+    routes: [{
+        path: "/"
+    }, {
+        path: "/:id",
+        component: () => import('./components/HelloWorld.vue')
+    }, {
+        path: "*",
+        component: () => import('./components/404.vue')
+    }]
+});
+
+export default router;
