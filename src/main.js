ca3dbba2e2534de261be8d467a9a56f45e345337
zhangtianli2006 <zhangtianli2006@163.com>
2020-06-29 18:45:48 +0800

    import font

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ import('element-ui').then(() => {
     Vue.use(Element);
 });
 
+import('./assets/fonts/Fira_Code/fira_code.css');
+import('./assets/fonts/Fira_Sans/fira_sans.css');
+import('./assets/fonts/fonts.css');
+
 new Vue({
     router,
 	render: h => h(App),
