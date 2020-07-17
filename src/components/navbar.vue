b353aa1c2d3adf44a5e19112a3876ed04ae0f65b
zhangtianli2006 <zhangtianli2006@163.com>
2020-07-17 12:11:57 +0800

    fix nav

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -5,31 +5,32 @@
       class="el-menu-demo"
       mode="horizontal"
       router="true"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
     >
       <el-menu-item index="/">首页</el-menu-item>
-      <el-menu-item>
-        <el-submenu>
-          <template slot="title">
-            <img src="./../assets/logo.png" id="user_icon"/>
-          </template>
-          <div v-if="this.$store.state.user.authenticated">
-            <el-menu-item>
-              <MyUserName />
-            </el-menu-item>
-            <el-menu-item index="/user/logout">
-              登出
-            </el-menu-item>
-          </div>
-          <div v-else>
-            <el-menu-item index="/user/login">
-              登录
-            </el-menu-item>
-            <el-menu-item index="/user/register">
-              注册
-            </el-menu-item>
-          </div>
-        </el-submenu>
-      </el-menu-item>
+      <el-submenu>
+        <template slot="title">
+          用户
+        </template>
+        <div v-if="this.$store.state.user.authenticated">
+          <el-menu-item>
+            <MyUserName />
+          </el-menu-item>
+          <el-menu-item index="/user/logout">
+            登出
+          </el-menu-item>
+        </div>
+        <div v-else>
+          <el-menu-item index="/user/login">
+            登录
+          </el-menu-item>
+          <el-menu-item index="/user/register">
+            注册
+          </el-menu-item>
+        </div>
+      </el-submenu>
     </el-menu>
   </div>
 </template>
@@ -51,6 +52,13 @@ export default {
 </script>
 
 <style lang="css" scoped>
+#nav {
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+}
+
 #user_icon {
   height: 50%;
 }
