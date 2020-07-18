72f8831cb85cf8832c3cb2e58721820954244ca3
szdytom <szdytom@163.com>
2020-07-18 09:49:18 +0800

    add problemlist

---
+++ src/components/problem/list.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="problem-list">
+    <AjaxTable
+      :ajax_url="this.ajax_url"
+      :columns="this.columns"
+      :limit=3
+      :total=10
+      ></AjaxTable>
+  </div>
+</template>
+
+<script>
+import apiurl from './../../apiurl';
+import AjaxTable from './../lib/AjaxTable.vue';
+
+export default {
+  name: 'ProblemList',
+  data() {
+    return {
+      ajax_url: apiurl('/problem/list'),
+      columns: [{
+        name: 'pid',
+        label: 'Problem ID'
+      }, {
+        name: 'title',
+        label: 'Title'
+      }]
+    }
+  },
+  components: {
+    AjaxTable
+  }
+};
+</script>
