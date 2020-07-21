24ceed4d0658c5071e4ef5615b5495a516d61e42
zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>
2020-07-21 15:11:08 +0800

    change format

--- src/loader/fonts.js
+++ src/loader/fonts.js
@@ -4,9 +4,9 @@
  */
 
 Promise.all([
-  import ('../assets/fonts/Fira_Code/fira_code.css'),
-  import ('../assets/fonts/Fira_Sans/fira_sans.css')
-])
-  .then(() => {
-    import ('../assets/fonts/fonts.css');
-  });
+        import ('../assets/fonts/Fira_Code/fira_code.css'),
+        import ('../assets/fonts/Fira_Sans/fira_sans.css')
+    ])
+    .then(() => {
+        import ('../assets/fonts/fonts.css');
+    });
