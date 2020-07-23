ecb3d2a980e5d86fef1afc862f76028995582d35
zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>
2020-07-23 19:25:18 +0800

    enable close button on messages
    Signed-off-by: zhangtianli2006 <49156174+zhangtianli2006@users.noreply.github.com>

--- src/components/user/logout.vue
+++ src/components/user/logout.vue
@@ -14,6 +14,7 @@ export default {
         .then(() => {
           this.$store.commit('userLogout');
           this.$message({
+            showClose: true,
             message: 'Logged out',
             type: 'success'
           });
