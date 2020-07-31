e866d2ca5fb83f04d142a8ca5c09fe314eb23292
gzezFISHER <fisher070305@163.com>
2020-07-31 15:27:53 +0800

    add tag

---
+++ src/components/lib/tag.vue
@@ -0,0 +1,67 @@
+<template>
+  <span :v-if="showTag" class="seg-tag" :style="style">
+    <i :v-if="icon" :class="'el-icon-' + icon_detail"></i>{{content}}
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'SegmentTag',
+  props: {
+    content: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: Boolean,
+      default: false
+    },
+    icon_detail: String,
+    border: {
+      type: Boolean,
+      default: true
+    },
+    color: {
+      type: String,
+      default: '#606266'
+    },
+    border_color: {
+      type: String,
+      default: '#E4E7ED'
+    },
+    background_color: {
+      type: String,
+      default: 'rgb (244, 255, 255)'
+    }
+  },
+  data() {
+    return {
+      style: '',
+      showTag: false
+    };
+  },
+  mounted() {
+    this.style = 'color: ' + this.color + '; background-color: ' + this.background_color + ';';
+    if(this.border) {
+      this.style += 'border-color: '+ this.border_color + ';';
+    }
+    this.showTag = true;
+  }
+};
+</script>
+
+<style scoped>
+.seg-tag {
+  display: inline-block;
+  height: 32px;
+  padding: 0 10px;
+  line-height: 30px;
+  font-size: 12px;
+  font-weight: 500;
+  border-width: 1px;
+  border-style: solid;
+  box-sizing: border-box;
+  white-space: nowrap;
+  margin: 3px;
+}
+</style>
