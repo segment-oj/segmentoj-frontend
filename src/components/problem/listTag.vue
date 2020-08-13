1411f271a7a7a8defa8116030540e04c5d90f5a9
gzezFISHER <fisher070305@163.com>
2020-08-13 15:10:15 +0800

    fix tags

--- src/components/problem/listTag.vue
+++ src/components/problem/listTag.vue
@@ -17,45 +17,64 @@
 <script>
 import SegmentTag from './../lib/tag.vue';
 import apiurl from './../../apiurl';
+import AWaitLock from './../../methods/lock';
 
 export default {
   name: 'listTag',
   data() {
     return {
-      rendertags: []
+      rendertags: new Array(),
     };
   },
   props: {
     tags: {
-      type: Array
+      type: Array,
+      required: true,
     },
   },
   watch: {
     tags() {
       this.loadTag();
-    }
+    },
   },
   methods: {
     loadTag() {
       this.rendertags = [];
-      for(let i = 0; i < this.tags.length; i += 1) {
-        this.$axios
-          .get(apiurl('/problem/tag/' + this.tags[i]))
-          .then(detail => {
-            let data = detail.data;
-            this.rendertags.push({
-              color: data.res.color,
-              content: data.res.content
-            });
+      let t = this.$store.state.tags;
+      for (let i = 0; i < this.tags.length; i += 1) {
+        if (t.tagsLock[this.tags[i]] === undefined) {
+          t.tagsLock[this.tags[i]] = new AWaitLock();
+        }
+        if (t.tagsData[this.tags[i]] !== undefined) {
+          this.rendertags[i] = t.tagsData[this.tags[i]];
+        } else {
+          t.tagsLock[this.tags[i]].acquire().then(() => {
+            if (t.tagsData[this.tags[i]] !== undefined) {
+              t.tagsLock[this.tags[i]].release();
+              this.rendertags[i] = t.tagsData[this.tags[i]];
+            } else {
+              this.$axios
+                .get(apiurl('/problem/tag/' + this.tags[i]))
+                .then((detail) => {
+                  let data = detail.data.res;
+                  t.tagsData[this.tags[i]] = {
+                    color: data.color,
+                    content: data.content,
+                  };
+                  this.rendertags[i] = t.tagsData[this.tags[i]];
+                  t.tagsLock[this.tags[i]].release();
+                });
+            }
           });
+        }
       }
-    }
+    },
   },
   mounted() {
     this.loadTag();
   },
   components: {
-    SegmentTag
-  }
+    SegmentTag,
+  },
 };
 </script>
