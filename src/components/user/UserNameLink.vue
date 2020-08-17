2b296dd31ce965d8b3158d1af31c28ad71d38f09
gzezFISHER <fisher070305@163.com>
2020-08-17 11:30:53 +0800

    add usernamelink

---
+++ src/components/user/UserNameLink.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <router-link :to="( '/account/' + String(this.userid))" class="color-regular-text text-normal">{{this.username}} </router-link>
+  </div>
+</template>
+
+<script>
+import apiurl from './../../apiurl';
+
+export default {
+  name: 'UserNameLink',
+  props: {
+    userid: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      username: new String()
+    };
+  },
+  methods: {
+    loadusername(id) {
+      this.$axios
+        .get(apiurl('/account/' + String(id)))
+        .then(res => {
+          this.username = res.data.res.username;
+        })
+        .catch(() => {
+          this.username = id;
+        });
+    }
+  },
+  mounted() {
+    console.log(typeof(this.userid));
+    this.username = this.userid;
+    this.loadusername(this.userid);
+  }
+};
+</script>
