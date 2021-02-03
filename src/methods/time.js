5371adabf90de003bfbc4eb1a50b0836ca4f149d
szdytom <szdytom@163.com>
2021-02-03 13:52:21 +0800

    [refactory] reduce use of internal variable

--- src/methods/time.js
+++ src/methods/time.js
@@ -1,7 +1,6 @@
 let timeFormat = (origin) => {
   let time = new Date(origin);
-  let res = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
-  return res;
+  return `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`;
 };
 
 export default timeFormat;
