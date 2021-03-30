616f01391372d42958c8ab8f71b433bbcf73cc64
zhangtianli2006 <zhangtianli2006@163.com>
2021-03-30 13:18:40 +0800

    [arch] remade submit page

---
+++ src/components/problem/submitPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <ProblemSubmit :pid="pid"></ProblemSubmit>
+</template>
+
+<script>
+import ProblemSubmit from './submit';
+
+export default {
+  data() {
+    return {
+      pid: this.$route.params.id,
+    };
+  },
+  components: {
+    ProblemSubmit,
+  }
+};
+</script>
